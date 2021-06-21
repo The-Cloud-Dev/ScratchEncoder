386c2fe4657c971c6ec35cc07df30c2fd5bb3fed
The-Cloud-Dev <85949157+The-Cloud-Dev@users.noreply.github.com>
2021-06-21 14:49:57 +0530

    Update main.py

--- ScratchEncoder/main.py
+++ ScratchEncoder/main.py
@@ -15,14 +15,10 @@ class Encoder():
                 logging.error('Character not supported')
         return encoded
     def decode(self, text):
-        print(text)
-        print(len(text)//2)
         decoded = ""
         y = 0
         for i in range(0,len(text)//2):
             x = chars[int(str(text[y])+str(text[int(y)+1]))-9]
-            print(y+1)
-            print(x)
             decoded = str(decoded)+str(x)
             y += 2
         return decoded
